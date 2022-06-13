9f794bbce20372a46d4176cbd05443f95e716646
Ben Chatelain <ben@ditto.live>
2022-06-13 16:50:00 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DittoPresenceViewer
+
+A description of this package.
---
+++ Sources/DittoPresenceViewer/DittoPresenceViewer.swift
@@ -0,0 +1,6 @@
+public struct DittoPresenceViewer {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
