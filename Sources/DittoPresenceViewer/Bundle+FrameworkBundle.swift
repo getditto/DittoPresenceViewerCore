747477538ed7256e01915ad34ba780537e8b73ec
Ben Chatelain <ben@ditto.live>
2022-09-22 20:32:52 -0600

    📦 Add Swift sources

---
+++ Sources/DittoPresenceViewer/Bundle+FrameworkBundle.swift
@@ -0,0 +1,13 @@
+//
+//  Copyright © 2020 DittoLive Incorporated. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+
+    static var presenceViewerFrameworkBundle: Bundle {
+        return Bundle(for: DittoPresenceView.self)
+    }
+
+}
