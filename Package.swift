15de3f79beaba2de539b72c305dd618ff8e4b6c7
Ben Chatelain <ben@ditto.live>
2022-09-22 20:35:26 -0600

    🔧 Limit to iOS platform for now

--- Package.swift
+++ Package.swift
@@ -4,7 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "DittoPresenceViewer",
-    platforms: [.macOS(.v11), .iOS(.v11)],
+    platforms: [
+        .iOS(.v11)
+        // .macOS(.v11)
+    ],
     products: [
         .library(
             name: "DittoPresenceViewer",
@@ -18,7 +21,13 @@ let package = Package(
             name: "DittoPresenceViewer",
             dependencies: [
                 .product(name: "DittoSwift", package: "DittoSwiftPackage")
-            ]),
+            ],
+            resources: [
+                .copy("Resources/index.html"),
+                .copy("Resources/main.css"),
+                .copy("Resources/main.js"),
+            ]
+        ),
         .testTarget(
             name: "DittoPresenceViewerTests",
             dependencies: ["DittoPresenceViewer"]),
