115ad5ffd68c4eedcb411ae5ed45a138494d996a
Ben Chatelain <ben@ditto.live>
2022-10-01 10:54:02 -0600

    ✨ Add Example project

---
+++ Example/DittoPresenceViewerExample/DittoPresenceViewerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DittoPresenceViewerExampleApp.swift
+//  DittoPresenceViewerExample
+//
+//  Created by Ben Chatelain on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct DittoPresenceViewerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
