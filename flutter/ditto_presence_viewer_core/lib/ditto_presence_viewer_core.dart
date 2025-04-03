0269085aa01c2bf844e61f08213f2756cd95029a
Brian Plattenburg <5767019+bplattenburg@users.noreply.github.com>
2025-04-03 14:30:26 -0400

    Adds some documentation

--- flutter/ditto_presence_viewer_core/lib/ditto_presence_viewer_core.dart
+++ flutter/ditto_presence_viewer_core/lib/ditto_presence_viewer_core.dart
@@ -1,5 +1,3 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+/// A Dummy dart class - this package serves to vend HTML & JS content only
+class DittoPresenceViewerCore {
 }
